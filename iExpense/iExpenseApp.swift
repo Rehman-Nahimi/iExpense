e4afaedf942471672317fac1e8c1f0a8db787fde
Rehman Nahimi <rehman2000@live.com>
2023-09-14 11:47:58 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ray Nahimi on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
